12c1f0e6840d5a3e3e09f85904d4654806fbd9ba
patildeep07 <patildeep07@gmail.com>
2022-08-05 15:10:40 +0530

    feat - added questions

---
+++ index.js
@@ -0,0 +1,27 @@
+// Import Library
+
+var readlineSync = require("readline-sync");
+
+// Defining the variables we are going to use
+
+var score = 0
+
+var questionList = [
+  {
+    question: "What is my name? ",
+    answer: "Deep"
+  },
+  {
+    question: "What is my age? ",
+    answer: "21"
+  },
+  {
+    question: "Where do I live? \n A. Kalwa \n B. Kalyan \n Choose the correct option (A or B)",
+    answer: "A"
+  },
+  {
+    question: "Am I still going to the gym? (yes or no) ",
+    answer: "yes"
+  }
+]
+
